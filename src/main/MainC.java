5b68551da7f1450695048c9fc518c44e3b9f8c64
sebastien <sebastien@openroam-prg-og-1-157-161.net.univ-paris-diderot.fr>
2018-03-16 16:06:45 +0100

    Commit des jours précédents

--- src/main/MainC.java
+++ src/main/MainC.java
@@ -1,4 +1,5 @@
 package main;
+
 import lejos.hardware.*;
 import lejos.robotics.*;
 import lejos.utility.Delay;
@@ -13,73 +14,66 @@ import lejos.remote.ev3.RemotePort;
 
 public class MainC {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		// Sound.beep();
-		
-		
+
 		try {
-		    LCD.clear();
-		    
+			LCD.clear();
 
 			Button.waitForAnyPress();
-		    
-			//Motors.motorTest();			
-			//Touch.touchTest();
-		    //Ultrason.ultrasonTest();
-		    //Colors.colorsTest();
-		    //Pliers.pliersTest();
-		    
-		    /***
-		    Pliers pliers = new Pliers(Motor.B);
-		    pliers.open();
-		    pliers.close();
-		    ***/
-		    
-		    /***
-		    ColorPreference pref = new ColorPreference("/home/lejos/preference/couleurs");
-		    pref.startCalibration();
-		    ***/
-		    
-		  
-		    
-			//while (!Button.ENTER.isDown()) {
 
-				Pliers pliers = new Pliers(Motor.B);
+			// Motors.motorTest();
+			// Touch.touchTest();
+			// Ultrason.ultrasonTest();
+			// Colors.colorsTest();
+			// Pliers.pliersTest();
 
-				Chassis chassis = new Chassis(Motor.A, Motor.C, 56, 68);
-				//pliers.open();
-				chassis.forward(300, 10000);				
-				LCD.drawString("Moteurs demarres", 1, 2);
-				//pliers.close();
-				//chassis.uTurn();	
-				//chassis.forward(300, 100);
-				//chassis.stop();
-				
-				EV3UltrasonicSensor ultrason = new EV3UltrasonicSensor(SensorPort.S3);
-				SampleProvider son= ultrason.getDistanceMode();
+			Pliers pliers = new Pliers(Motor.B);
 
-		        float[] sample = new float[son.sampleSize()];
-		        float range=10;
-		        while (range>0.8){
-		        	son.fetchSample(sample, 0);
-			        range=sample[0];
-			        LCD.drawString("distance: "+ range, 1, 2);
-			    }
-		        chassis.stop();
-		        Button.waitForAnyPress();
+			/***
+			 * ColorPreference pref = new
+			 * ColorPreference("/home/lejos/preference/couleurs"); pref.startCalibration();
+			 ***/
 
-			//}
-		    
+			// while (!Button.ENTER.isDown()) {
+
+			Chassis chassis = new Chassis(Motor.A, Motor.C, 56, 68);
+			// pliers.open();
+			chassis.forward(300, 10000);
+			/**
+			 * Il faudrait qu'on fasse varier la vitesse
+			 */
+			LCD.drawString("Moteurs demarres", 1, 2);
+			// pliers.close();
+			// chassis.uTurn();
+			// chassis.forward(300, 100);
+			// chassis.stop();
+
+			Ultrason ultrason = new Ultrason(SensorPort.S3);
+			SampleProvider son = ultrason.getDistanceMode();
+
+			float[] sample = new float[son.sampleSize()];
+			float range = 1;
+			pliers.open();
+			Ultrason ul = new Ultrason();
 			
+			do {
+				LCD.clear();
+				LCD.drawString("distance:", 1, 2);
+				LCD.drawString("" + range, 1, 3);
+			} while (ultrason.detectionPalet());
+			LCD.drawString("" + range, 1, 3);
+			chassis.stop();
+			pliers.close();
+			Button.waitForAnyPress();
+
+			// }
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			Button.waitForAnyPress();
 		}
-	
+
+	}
+
 }
-	
-}
-
-
-
-
--- src/main/Ultrason.java
+++ src/main/Ultrason.java
@@ -1,41 +1,80 @@
 package main;
 
 import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
+import lejos.hardware.port.UARTPort;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
-public class Ultrason {
+public class Ultrason extends EV3UltrasonicSensor{
 	
 	public static EV3UltrasonicSensor eyes;
 	public static SampleProvider view;
+	private float[] sample;
+	private float range;
 	
-	public Ultrason(BaseRegulatedMotor motorPort){
-		this.eyes = new EV3UltrasonicSensor((Port) motorPort);
-		this.view= eyes.getDistanceMode();
+	public Ultrason(){
+		this("S2");
 	}
 	
+	public Ultrason(Port port) {
+		super(port);
+		this.view = this.getDistanceMode();
+		sample = new float[view.sampleSize()];
+		range = 1;
+	}
+
+	public Ultrason(String portName){
+		super(LocalEV3.get().getPort(portName));
+		this.view = this.getDistanceMode();
+		sample = new float[view.sampleSize()];
+		range = 1;
+	}
 	
+	public boolean detectionPalet() {
+		/**
+		 * 1. On capte qqchs
+		 * 2. Si on ne recapte plus, on ouvre les pinces
+		 * 3. Sinon, on referme les pinces
+		 */
+		
+		/**
+		 * Etape 1
+		 */
+		while (range > 0.4) {
+			view.fetchSample(sample, 0);
+			range = sample[0];
+			
+		}
+		Delay.msDelay(200);
+		/**
+		 * Etape 2
+		 */
+		view.fetchSample(sample, 0);
+		range = sample[0];
+		return range > 0.4;
+	}
 	
 	/***
-public static void ultrasonTest(){
-		
-		EV3UltrasonicSensor ultrason = new EV3UltrasonicSensor(SensorPort.S3);
-		SampleProvider son= ultrason.getDistanceMode();
-
-        float[] sample = new float[son.sampleSize()];
-        float range=10;
-        Motor.A.forward();
-    	Motor.C.forward();
-        while (range>0.1){
-        	son.fetchSample(sample, 0);
-	        range=sample[0];
-	    }
+	 * public static void ultrasonTest(){
+	 * EV3UltrasonicSensor ultrason = new EV3UltrasonicSensor(SensorPort.S3);
+	 * SampleProvider son= ultrason.getDistanceMode();
+	 * float[] sample = new float[son.sampleSize()];
+	 * float range=10;
+	 * Motor.A.forward();
+	 * Motor.C.forward();
+	 * while (range>0.1){
+	 * 		son.fetchSample(sample, 0);
+	 * 		range=sample[0];
+	 * }
         Motor.A.stop();
     	Motor.C.stop();	
 	    LCD.drawString("touchTest Finished", 3, 4);
